21e0f545dca8c5ff6276e1edfc7e1f7c7d5f697b
Valeriu Saros <valeriu.saros@gmail.com>
2020-12-01 21:45:00 +0200

    Added the Login Page

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Login from './Login';
+
+function App() {
+  return (
+    // BEM
+    <div className="app">
+
+      <Login />
+
+    </div>
+  );
+}
+
+export default App;
